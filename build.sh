66151f43d310e65d1a9b5e767a5873cf99dab54f
Daniel Huisman <daniel@huisman.me>
2021-10-21 18:16:34 +0200

    Split WebAssembly build into browser and Node.js

--- build.sh
+++ build.sh
@@ -4,15 +4,25 @@ set -e
 # Apply patch
 patch -p0 -f < Makefile.patch
 
-# Build WebAssembly
+# Configure Yosys for WebAssembly build
 cd yosys
 make config-emcc
+
+# Build browser version
 make -j $(nproc)
-sed -i 's|var FS=|var FS=Module.FS=|' yosys.js
+mv yosys.js yosys.browser.js
+
+# Build Node.js version (mostly cached)
+make EMCC_LDLIBS=-lnodefs.js -j $(nproc)
+mv yosys.js yosys.node.js
+
+# Fix FS exports
+sed -i 's|var FS=|var FS=Module.FS=|' yosys.*.js
 cd ..
 
 # Publish npm package
 yarn run build
 
 # Undo patch
+# TODO: always execute after patching, even if previous commands failed
 patch -p0 -f -R < Makefile.patch
